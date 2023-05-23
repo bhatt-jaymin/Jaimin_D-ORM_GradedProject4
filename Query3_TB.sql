8d8bbd3ad573959aad5e3683a96862cc1c67baff
bhatt-jaymin <bhatt.jaymin@gmail.com>
2023-05-23 15:54:24 +0530

    Delete Query3_TB.sql

--- Query3_TB.sql
+++
@@ -1,7 +0,0 @@
--- 3) Display the total number of customers based on gender who have placed orders of worth at least Rs.3000.
-
-SELECT cus.CUS_GENDER, COUNT(*) AS Total_Customers
-FROM customer cus
-INNER JOIN orders ord ON cus.CUS_ID = ord.CUS_ID
-WHERE ord.ORD_AMOUNT >= 3000
-GROUP BY cus.CUS_GENDER;
